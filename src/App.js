c4d8c2953cc0c0ba0c7201f567cc9bb98c440f65
tayfunetta <tayfunemrekahraman@gmail.com>
2022-06-20 19:16:47 +0300

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { createContext, useContext } from 'react'
+import { data } from './data';
+
+const myContext = createContext()
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='container'>
+      <div id='inner-container'>
+        <h1>Our Tours</h1>
+        <myContext.Provider value={data}>
+          {data.map((elem, index) => <Item key={index} index={index} />)}
+        </myContext.Provider>
+      </div>
+      <footer>
+        <div id='footer-left'><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati harum repellat, doloribus commodi architecto iure. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati harum repellat, doloribus commodi architecto iure?</p></div>
+        <div id='footer-right'><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati harum repellat, doloribus commodi architecto iure?</p></div>
+      </footer>
     </div>
   );
 }
+
+const Item = props => {
+  const context = useContext(myContext)
+  const obj = context[props.index]
+  return (
+    <div className='item'>
+      <img src={obj.photo} alt={obj.city}></img>
+      <div className='info'>
+        <div className='info-top'>
+          <span className='title'>{obj.city}</span>
+          <span className='price'>${obj.price}</span>
+        </div>
+        <p>
+          {obj.info}
+        </p>
+        <button onClick={event => { event.target.parentNode.parentNode.remove() }}>Not Interested</button>
+      </div>
+    </div>
+  );
+}
+
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,17 @@
+import place1 from './images2/place1.jpg'
+import place2 from './images2/place2.jpeg'
+import place3 from './images2/place3.jpeg'
+import place4 from './images2/place4.jpeg'
+import place5 from './images2/place5.jpeg'
+import place6 from './images2/place6.png'
+
+const lorem = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, voluptatum voluptates odio architecto placeat rerum sit! Sit ullam perferendis atque impedit iste quibusdam animi neque, tempora ut enim magni aliquid dolorem libero nisi officiis dolore quis quasi eligendi iusto veniam?'
+
+export const data = [
+    {photo: place1, city: 'İstanbul', price: 1500, info: lorem},
+    {photo: place2, city: 'London', price: 2000, info: lorem},
+    {photo: place3, city: 'New York', price: 2200, info: lorem},
+    {photo: place4, city: 'Berlin', price: 3000, info: lorem},
+    {photo: place5, city: 'Tokyo', price: 4000, info: lorem},
+    {photo: place6, city: 'Lyon', price: 3750, info: lorem},
+  ]
